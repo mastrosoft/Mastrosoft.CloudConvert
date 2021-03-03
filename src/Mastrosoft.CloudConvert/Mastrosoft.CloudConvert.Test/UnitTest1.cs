96634ec3a8f436ddc20f13ed99282912b6e74e5a
Sandro Mastronardi <sandro.mastronardi@mastrosoft.com>
2021-03-03 11:02:48 +0100

    add test project

---
+++ src/Mastrosoft.CloudConvert/Mastrosoft.CloudConvert.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mastrosoft.CloudConvert.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
